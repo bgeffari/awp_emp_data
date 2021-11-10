93233051b8b37587e863db9bf584f8b14569c578
bgeffari <bgeffari@gmail.com>
2021-11-10 11:55:08 +0300

    fix ID

--- app/Http/Controllers/Frontend/SubCertificateTypesController.php
+++ app/Http/Controllers/Frontend/SubCertificateTypesController.php
@@ -92,7 +92,7 @@ class SubCertificateTypesController extends Controller
         $html = '<option value="">'.trans('global.pleaseSelect').'</option>';
     } else {
         $html = '';
-        $SubCertificateTypes = SubCertificateType::where('main_certificate_type_id', $request->main_certificate_type_id)->get();
+        $SubCertificateTypes = SubCertificateType::where('id', $request->main_certificate_type_id)->get();
         foreach ($SubCertificateTypes as $SubCertificateType) {
             $html .= '<option value="'.$SubCertificateType->id.'">'.$SubCertificateType->sub_certificate_types.'</option>';
         }
